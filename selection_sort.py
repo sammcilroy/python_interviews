b2a616aac4f22bfa4269afcea8b0f52bfdc40fe0
Sam McIlroy <sam.mcilroy@gmail.com>
2020-04-11 12:43:16 +0100

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,22 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+#print(findSmallest([4,3,5,7,9,1,12]))
+#print(findSmallest([1,6,9,4,5,8,2]))
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        # find the smallest element in the array
+        smallest = findSmallest(arr)
+        # add it to the new array
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5,3,6,2,10]))
