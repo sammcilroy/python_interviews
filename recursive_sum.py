e5ebd16763c2e8b5a2bdd609b1b331c4bd5699fa
Sam McIlroy <sam.mcilroy@gmail.com>
2020-04-14 12:33:00 +0100

    recursive sum

---
+++ recursive_sum.py
@@ -0,0 +1,22 @@
+def sum(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
+
+#print(sum([2,4,6]))
+
+def recursive_sum(arr):
+    # BASE CASE - length of array is zero or 1
+    if len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return sum(arr)
+    # RECURSIVE CASE - sum
+    return arr[0] + sum(arr[1:])
+
+
+
+#print(recursive_sum([]))
+
+print(recursive_sum([2,4,6]))
