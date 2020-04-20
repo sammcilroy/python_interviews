02f5f41778b3fe10c29ce54c5560d31618dcac08
Sam McIlroy <sam.mcilroy@gmail.com>
2020-04-20 14:45:52 +0100

    complete function

--- mango.py
+++ mango.py
@@ -12,13 +12,22 @@ graph["thom"] = []
 graph["johnny"] = []
 
 from collections import deque
-search_queue = deque() # create a new queue
-search_queue += graph["you"] # add all of my neighbours to the queue
 
-while search_queue: # while the queue is not empty
-    person = search_queue.popleft() # take the first person from the queue
-    if person_is_seller(person): # check whether the person is a mango seller
-        print(person + " is a mango seller!")
-        break
-    else:
-        search_queue += graph[person] # not a seller, add all of their neighbours the queue
+def search(name):
+    search_queue = deque() # create a new queue
+    search_queue += graph[name] # add all of my neighbours to the queue
+    searched = [] # keep track of which people have been searched
+
+    while search_queue: # while the queue is not empty
+        person = search_queue.popleft() # take the first person from the queue
+        if not person in searched:
+            if person_is_seller(person): # check whether the person is a mango seller
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person] # not a seller, add all of their neighbours the queue
+                searched.append(person)
+
+    return False
+
+search("you")
